b1f85e5d55c4f13cc2cfdda335e9ddc580ff46c2
Joshua Kaiser <jmk@Mini-von-Joshua.fritz.box>
2024-03-06 10:23:59 +0100

    Das längste Wort wird jetzt gefunden und ausgegeben

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 //Textanalyse-Tool
 
-const readlineSync = require('readline-sync');
+const readlineSync = require("readline-sync");
 
-const text = readlineSync.question("Gib einen belibigen Text ein und ich analysiere ihn dir. ")
-const gesammtAnzahl = wörterZählen(text)
+const text = readlineSync.question(
+  "Gib einen belibigen Text ein und ich analysiere ihn dir. "
+);
+const wörter = text.split(/\s+/);
+
+const gesammtAnzahl = wörterZählen(text);
 console.log(`Du hast insgesammt ${gesammtAnzahl} wörter geschrieben`);
 
-const unterschiedlicheWörter = unterschiedlicheWörterZählen(text)
+const unterschiedlicheWörter = unterschiedlicheWörterZählen(text);
 console.log(`Davon waren ${unterschiedlicheWörter} unterschiedliche Wörter`);
 
+const längstesWort = wörter.reduce(function (a, b) {
+  return a.length > b.length ? a : b;
+});
+console.log(`und das längste Wort in deinem Text ist: "${längstesWort}"`);
+
 function wörterZählen(text) {
-    const wörter = text.split(/\s+/);
-    return wörter.length;
+  const wörter = text.split(/\s+/);
+  return wörter.length;
 }
 
 function unterschiedlicheWörterZählen(text) {
-    const wörter = text.split(/\s+/);
-    const unterschiedlicheWörter = new Set(wörter);
-    return unterschiedlicheWörter.size;
+  const wörter = text.split(/\s+/);
+  const unterschiedlicheWörter = new Set(wörter);
+  return unterschiedlicheWörter.size;
 }
